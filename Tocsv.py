b066cd3cff890f9422efa06e082d0e7d240577b0
GearlessJohn <eloiwangxuan@qq.com>
2022-06-28 14:49:06 +0200

    first database

---
+++ Tocsv.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import pandas as pd
+
+data = pd.read_json('nodes.json').T
+data = data.reset_index()
+print("Showing the first ten rows:")
+print(data.head(10))
+data.to_csv("nodes.csv", encoding='UTF-8')
