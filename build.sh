eeb75737694ad641443c276fb2e8ef67420f83a6
GearlessJohn <eloiwangxuan@qq.com>
2022-07-13 15:35:57 +0200

    first version with json format output

--- build.sh
+++ build.sh
@@ -5,9 +5,5 @@ set -e
 # Create necessary folder
 mkdir -p rawdata
 
-export NIXPKGS_ALLOW_INSECURE=1
-export NIXPKGS_ALLOW_UNFREE=1
-export NIXPKGS_ALLOW_UNSUPPORTED_SYSTEM=1
 # Get all nodes and edges information
-echo -e $(nix-instantiate --eval --json --strict --keep-going default.nix -A info1) >rawdata/edges.json
-# echo -e $(nix-instantiate --eval default.nix -A info1) >rawdata/edges.json
+echo -e $(nix-instantiate --eval --json --strict --keep-going default.nix -A info) >rawdata/edges.json
