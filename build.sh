655b36d725ca26979adc4924c2475d732ad4a5db
GearlessJohn <eloiwangxuan@qq.com>
2022-07-21 13:50:29 +0200

    3rd try of recursive func with isDerivation

--- build.sh
+++ build.sh
@@ -4,6 +4,5 @@ set -e
 
 # Create necessary folder
 mkdir -p rawdata
-export NIXPKGS_ALLOW_UNFREE=1
 # Get all nodes and edges information
-echo -e $(nix-instantiate --eval --json --strict --keep-going default.nix -A info) >rawdata/edges.json
+echo -e $(nix-instantiate --eval --json --strict --show-trace default.nix -A info) >rawdata/edges1.json
