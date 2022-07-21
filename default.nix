7c91907d1bfbba0e45c365e0575fccb79ce58276
GearlessJohn <eloiwangxuan@qq.com>
2022-07-21 10:01:20 +0200

    first try of recurse fucntion

--- default.nix
+++ default.nix
@@ -1,33 +1,39 @@
-{
-  pkgs ? import (fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/59b5ae59892ff16075bab39a7d6a9c8509b4055f.tar.gz";
-    sha256 = "0c9yjk5spc8mkq5rqcql6j8mqmlq62299l3cz29pjvgxwazwwpv0";
-  }) {}
+{ pkgs ? import
+    (fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/59b5ae59892ff16075bab39a7d6a9c8509b4055f.tar.gz";
+      sha256 = "0c9yjk5spc8mkq5rqcql6j8mqmlq62299l3cz29pjvgxwazwwpv0";
+    })
+    { }
 }:
 
 with pkgs;
 
-let 
+let
   inherit (pkgs) lib;
 
   inherit (builtins) tryEval;
   concatString = lib.concatMapStrings (x: (builtins.toString x) + " ");
 
-# Here is the function that we use to extract information from one package. This function takes in a package and returns an attrset. 
-# name : the name of the package (key); value : the information of the package in set form
-# Note that variables like buildInputs (set or list) need to be converted to string format first. 
-# Otherwise, you will encounter various types of errors with `nix-instantiate --eval --json --strict`
-  extractInfo = name: value:
-    let 
-      res = {
-        name = (tryEval(if value ? name then value.name else "")).value;
-        path = (tryEval(if value ? outPath then value.outPath else "")).value;
-        buildInputs = (tryEval(if value ? buildInputs then concatString value.buildInputs else "")).value;
-      };
-    in 
-      res;
+  # Here is the function that we use to extract information from one package. This function takes in a package and returns an attrset. 
+  # 
+  # @name : the name of the package (key); @value : the information of the package in set form
+  # Note that variables like buildInputs (set or list) need to be converted to string format first. 
+  # Otherwise, you will encounter various types of errors with `nix-instantiate --eval --json --strict`
+  extractInfo = depth: pathName: lib.mapAttrs (
+    name: value:
+      if (builtins.typeOf value) == "set" then
+        extractInfo (depth + 1) ''${pathName}.${n}'' v
+      else
+        {
+          name = (tryEval (if value ? name then value.name else "")).value;
+          inherit depth pathName;
+          path = (tryEval (if value ? outPath then value.outPath else "")).value;
+          buildInputs = (tryEval (if value ? buildInputs then concatString value.buildInputs else "")).value;
+        }
+  );
 
-in rec {
-  info = (lib.mapAttrs extractInfo) pkgs;
+in
+rec {
+  info = extractInfo 0 "nixpkgs" pkgs;
 }
 
