99d99dfc43dc14174c80f6b6799b3ac6f11c80bd
GearlessJohn <eloiwangxuan@qq.com>
2022-08-05 16:18:24 +0200

    activate json file generating

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ set -e
 mkdir -p rawdata
 
 # Get all nodes and edges information
-# echo -e $(nix-instantiate --eval --json --strict --show-trace default.nix -A info) >rawdata/nodes.json
+echo -e $(nix-instantiate --eval --json --strict --show-trace default.nix -A info) >rawdata/nodes.json
 
 # Generate the first graph
 nix-shell shell.nix --run "
