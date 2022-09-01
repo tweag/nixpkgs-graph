bffd5d2ad5524832f7d785b817bf649a4e436b4b
GearlessJohn <eloiwangxuan@qq.com>
2022-09-01 17:27:01 +0200

    fix build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,9 @@ set -e
 
 # Generate and analyze the graph
 nix-shell shell.nix --run "
+    python -m venv .venv
     source .venv/bin/activate
+    pip install -e .
     python -m nixpkgs_graph build --rev $1 --sha256 $2 --output ./rawdata/nodes.json
     python -m nixpkgs_graph generate-graph --input-file ./rawdata/nodes.json --output-folder ./rawdata/
     cp ./rawdata/first_graph.graphml \$NEO4J_HOME/share/neo4j/import/
