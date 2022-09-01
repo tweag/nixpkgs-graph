ee0adbf0f0a3b8ede9964ec7cf97cbbe7a8ceb83
GearlessJohn <eloiwangxuan@qq.com>
2022-09-01 17:11:29 +0200

    build.sh with neo4j

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+# Generate and analyze the graph
+nix-shell shell.nix --run "
+    source .venv/bin/activate
+    python -m nixpkgs_graph build --rev $1 --sha256 $2 --output ./rawdata/nodes.json
+    python -m nixpkgs_graph generate-graph --input-file ./rawdata/nodes.json --output-folder ./rawdata/
+    cp ./rawdata/first_graph.graphml \$NEO4J_HOME/share/neo4j/import/
+    cypher-shell -a bolt://localhost:7687 \"match (n) detach delete n\"
+    cypher-shell -a bolt://localhost:7687 \"call apoc.import.graphml('first_graph.graphml', {})\"
+"
--- shell.nix
+++ shell.nix
@@ -1,4 +1,6 @@
-{ pkgs ? import <nixpkgs> { } }:
+{ pkgs ? import <nixpkgs> { }, pkgs1 ?
+  import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/master.tar.gz")
+  { } }:
 let
   my-python = pkgs.python3;
   my-pip = pkgs.python3Packages.pip;
@@ -8,19 +10,24 @@ let
       networkx
       pandas
       scipy
-      numpy
       click
       setuptools
       # other python packages needed here
     ]);
+  neo4j = pkgs1.neo4j;
 in pkgs.mkShell {
   buildInputs = [
     pythonEnv
     my-pip
+    neo4j
     # other dependencies needed here
   ];
   shellHook = ''
     PYTHONPATH=${pythonEnv}/${pythonEnv.sitePackages}
+    NEO4J_HOME=${neo4j}
+    chmod 755 -R $NEO4J_HOME/share/neo4j/
+    wget -P $NEO4J_HOME/share/neo4j/plugins/ -nc https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/download/4.4.0.1/apoc-4.4.0.1-all.jar
+    echo "apoc.import.file.enabled=true" >> $NEO4J_HOME/share/neo4j/conf/neo4j.conf
     # maybe set more env-vars
   '';
 }
